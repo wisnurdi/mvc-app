5cf0487487bc7e1ccc050db727a5b632973e4c11
Wisnu <wisnurdi@gmail.com>
2018-02-20 09:19:21 +0700

    Create about.php

---
+++ V/about.php
@@ -0,0 +1,12 @@
+<div class="header">
+    <h1>About</h1>
+    <h2>About this framework</h2>
+</div>
+
+<div class="content">
+    <h2 class="content-subhead">This is a simple framework</h2>
+    <p>
+        PHP framework is a thing. This is a kind of PHP framework which follows design patterm of MVC.
+    </p>
+
+</div>
