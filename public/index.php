e4c685bf7ec2cee23822aa8a3561ec35f0368c4a
Wisnu <wisnurdi@gmail.com>
2018-02-20 09:21:39 +0700

    Create index.php

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+
+// date_default_timezone_set("Asia/Jakarta");
+// setlocale(LC_ALL, 'IND');
+setlocale(LC_ALL, 'id_ID.UTF8', 'id_ID.UTF-8', 'id_ID.8859-1', 'id_ID', 'IND.UTF8', 'IND.UTF-8', 'IND.8859-1', 'IND', 'Indonesian.UTF8', 'Indonesian.UTF-8', 'Indonesian.8859-1', 'Indonesian', 'Indonesia', 'id', 'ID', 'en_US.UTF8', 'en_US.UTF-8', 'en_US.8859-1', 'en_US', 'American', 'ENG', 'English');
+
+ini_set('max_execution_time', 300);
+
+DEFINE('DIR', dirname(__DIR__) . '/'); //harus disetting seperti ini
+
+require DIR . 'vendor/autoload.php';
+require DIR . 'app/config.php';
+
+$app = new mvc\Mvc($config);
+$app->run();
