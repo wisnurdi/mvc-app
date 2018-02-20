adf2338241c1a004293320547e01e3579f68ea4f
Wisnu <wisnurdi@gmail.com>
2018-02-20 09:21:02 +0700

    Create contact.php

---
+++ V/contact.php
@@ -0,0 +1,18 @@
+<div class="header">
+    <h1>Contact</h1>
+</div>
+<div class="content">
+	<form class="pure-form">
+	    <fieldset class="pure-group">
+	        <input type="text" class="pure-input-1-2" placeholder="name">
+	        <input type="email" class="pure-input-1-2" placeholder="email">
+	    </fieldset>
+
+	    <fieldset class="pure-group">
+	        <input type="text" class="pure-input-1-2" placeholder="A title">
+	        <textarea class="pure-input-1-2" placeholder="Komentar"></textarea>
+	    </fieldset>
+
+	    <button type="submit" class="pure-button pure-input-1-2 pure-button-primary">Submit</button>
+	</form>
+</div>
